6c29ab04315b14e95689475d7047bd1f172a9583
natreeum <projectkmscom@gmail.com>
2023-01-03 10:16:09 +0900

    first commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+
+const apiRouter = require('./router/apiroot');
+
+const http = require('http').createServer(app);
+http.listen(8080, () => {
+  console.log('Listening 8080');
+});
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api', apiRouter);
