14428f6e9ccf97b9b5738fb16db31099f4c24863
natreeum <projectkmscom@gmail.com>
2023-01-03 16:46:34 +0900

    feat : selComment without contract

--- server/router/apiroot.js
+++ server/router/apiroot.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const selComment = require('../function/selcomment');
+
 router.get('/', (req, res) => {
   res.send('This is api router');
+});
+
+router.post('/selcomment', (req, res) => {
+  selComment(req, res);
 });
 
 module.exports = router;
