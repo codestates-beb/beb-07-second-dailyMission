139964506ca91f19b3b4e25869cf142868b0a23f
bbabi0901 <bbabi0901@gmail.com>
2023-01-04 22:22:36 +0900

    fix: 'res.send()' to 'return res.status()' in commentController.js

--- server/controller/commentController.js
+++ server/controller/commentController.js
@@ -4,12 +4,23 @@ module.exports = {
   newComment: async (req, res) => {
     try {
       const { missionId, userId, content, ipfsHash } = req.body;
+      if (!missionId || !userId || !content || !ipfsHash) {
+        return res.status(400).send({
+          status: "failed",
+          message:
+            "Invalid request body. Need missionId, userId, content, ipfsHash",
+        });
+      }
       const data = { missionId, userId, content, ipfsHash, isSelected: false };
       const newCommentRes = await prisma.comment.create({ data: data });
-      res.send({ status: "success", message: newCommentRes });
+      return res
+        .status(200)
+        .send({ status: "success", message: newCommentRes });
     } catch (e) {
       console.log(e);
-      res.send({ status: "failed", message: "Failed in commenting" });
+      return res
+        .status(400)
+        .send({ status: "failed", message: "Failed in commenting" });
     }
   },
 };
