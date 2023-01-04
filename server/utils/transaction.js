29b32b09992062d8249f9335ca37cb5b9f0546fa
natreeum <projectkmscom@gmail.com>
2023-01-04 20:31:17 +0900

    fix : tokenTransfer return value fix

--- server/utils/transaction.js
+++ server/utils/transaction.js
@@ -1,11 +1,11 @@
-const Web3 = require("web3");
-const ERC20abi = require("./abi/ERC20abi");
-const { ethBalance, tokenBalance } = require("./wallet");
-const { getPasswordByAddr } = require("./utils");
-require("dotenv").config({ path: "../.env" });
+const Web3 = require('web3');
+const ERC20abi = require('./abi/ERC20abi');
+const { ethBalance, tokenBalance } = require('./wallet');
+const { getPasswordByAddr } = require('./utils');
+require('dotenv').config({ path: '../.env' });
 
 const { ERC20_ADDRESS, ADMIN_ADDRESS, ADMIN_PRIVATEKEY } = process.env;
-const web = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:7545"));
+const web = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'));
 const tokenContract = new web.eth.Contract(ERC20abi, ERC20_ADDRESS);
 
 const isEthEnough = async (from, gas) => {
@@ -23,20 +23,20 @@ const isTokenEnough = async (from, amount) => {
 const sendTokenGanache = async (from, to, amount) => {
   try {
     if (!isTokenEnough) {
-      return { status: false, message: "Not enough token to send" };
+      return { status: false, message: 'Not enough token to send' };
     }
     const data = await tokenContract.methods.transfer(to, amount);
     const gas = await data.estimateGas();
     if (!isEthEnough(from, gas)) {
-      return { status: false, message: "Not enough ethereum to pay gas" };
+      return { status: false, message: 'Not enough ethereum to pay gas' };
     }
     const result = await data.send({ from: from });
     return {
       status: result.status,
       message:
         from === ADMIN_ADDRESS
-          ? "Rewarded successfully"
-          : "Sended successfully",
+          ? 'Rewarded successfully'
+          : 'Sended successfully',
     };
   } catch (e) {
     console.log(e);
@@ -49,13 +49,13 @@ module.exports = {
     await sendTokenGanache(ADMIN_ADDRESS, to, amount),
   sendTokenGanache: async (from, to, amount) => {
     const password = await getPasswordByAddr(from);
-    if (!password) return { status: false, message: "Address Not Found." };
+    if (!password) return { status: false, message: 'Address Not Found.' };
     const unlock = await web.eth.personal.unlockAccount(from, password, 3);
     if (unlock) {
       const giveRes = await sendTokenGanache(from, to, amount);
       await web.eth.personal.lockAccount(from);
-      return giveRes;
-    } else return { status: false, message: "Failed to unlock address" };
+      return { status: true, message: giveRes };
+    } else return { status: false, message: 'Failed to unlock address' };
   },
   // rewardTokenGoerli: async (to, amount) =>
   //   await sendTokenGoerli(ADMIN_ADDRESS, to, amount),
@@ -89,12 +89,12 @@ const signTx = async (tx) => {
     const hash = web.eth.sendSignedTransaction(
       txSigned.rawTransaction,
       (err, hash) => {
-        if (err) console.log("Transaction Error:", err);
+        if (err) console.log('Transaction Error:', err);
       }
     );
     return hash;
   } catch (err) {
-    console.log("Promise Error:", err);
+    console.log('Promise Error:', err);
     return false;
   }
 };
