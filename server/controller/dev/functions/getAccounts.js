bdeb4928320f59e44cc07c1fb7cfafd091a75336
natreeum <projectkmscom@gmail.com>
2023-01-05 15:00:29 +0900

    fix : /api/dev/accounts response edit

--- server/controller/dev/functions/getAccounts.js
+++ server/controller/dev/functions/getAccounts.js
@@ -1,16 +1,25 @@
 require('dotenv').config();
 
-const { getWeb3 } = require('../../../utils/web3');
+const { getWeb3, getContract } = require('../../../utils/web3');
 
 // fetch env variables
 const env = process.env;
 const network = env.LOCAL_RPC_SERVER_NETWORK;
 const port = env.LOCAL_RPC_SERVER_PORT;
+const contractAddress = env.CONTRACT_ADDRESS;
+
+const abi = require('../../../utils/abi/ICTokenABI');
 
 const getAccounts = async (req, res) => {
-  const web3 = await getWeb3(network, port);
+  const web3 = getWeb3(network, port);
+  const contract = getContract(web3, abi, contractAddress);
   const accounts = await web3.eth.getAccounts();
-  res.status(200).send(accounts);
+  const accountsWithBalance = {};
+  for (let i of accounts) {
+    const balance = await contract.methods.balanceOf(i).call();
+    accountsWithBalance[i] = balance;
+  }
+  res.status(200).send(accountsWithBalance);
 };
 
 module.exports = { getAccounts };
