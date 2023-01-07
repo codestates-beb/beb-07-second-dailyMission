57e6827ee13813d95aff2cc58cb5d5e0cef33bfc
natreeum <projectkmscom@gmail.com>
2023-01-08 05:18:30 +0900

    feat : pagenation

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -8,6 +8,7 @@ import './Landing.css';
 
 const Landing = () => {
   const [missions, setMissions] = useState([]);
+  const [curPage, setPage] = useState(0);
   const getMissions = () => {
     axios.get(`${apiUrl}/missions`).then((e) => {
       e.data.status === 'success'
@@ -15,33 +16,55 @@ const Landing = () => {
         : setMissions([]);
     });
   };
-
+  const missionsPage = missions.slice(curPage, curPage + 10);
+  const nextPage = () => {
+    if (Math.floor(missions.length / 10) === Math.floor(curPage / 10)) return;
+    else setPage(curPage + 10);
+  };
+  const prevPage = () => {
+    if (Math.floor(curPage / 10) === 0) return;
+    else setPage(curPage - 10);
+  };
   useEffect(() => {
     getMissions();
   }, []);
 
   return (
-    <div className="Landing" align="center">
-      <div>This is Landing</div>
-      <button>prev page</button>
-      <button>next page</button>
-      <button>new mission</button>
-      <div className="missionHeader">
-        <div>ID</div>
-        <div>R/C</div>
-        <div>Reward</div>
-        <div>Title(selected/comments)</div>
-        <div>Author</div>
-        <div>CreatedAt</div>
-      </div>
-      <div className="missionsContainer" align="center">
-        {missions.length !== 0 ? (
-          missions.map((e) => {
-            return <Mission key={missions.indexOf(e)} message={e} />;
-          })
-        ) : (
-          <div className="noMissions">No missions</div>
-        )}
+    <div align="center">
+      <div className="Landing">
+        <div>This is Landing</div>
+        <div className="buttonContainer">
+          <button className="prevPage" onClick={prevPage}>
+            prev page
+          </button>
+          <div className="curPage">
+            {Math.floor(curPage / 10) + 1}/
+            {Math.floor(missions.length / 10) + 1}
+          </div>
+          <button className="nextPage" onClick={nextPage}>
+            next page
+          </button>
+          <button className="newMission">new mission</button>
+        </div>
+        <div className="missionHeader">
+          <div>ID</div>
+          <div>R/C</div>
+          <div>Reward</div>
+          <div>Title(selected/comments)</div>
+          <div>Author</div>
+          <div>
+            CreatedAt<br></br>EndAt
+          </div>
+        </div>
+        <div className="missionsContainer" align="center">
+          {missions.length !== 0 ? (
+            missionsPage.map((e) => {
+              return <Mission key={missions.indexOf(e)} message={e} />;
+            })
+          ) : (
+            <div className="noMissions">No missions</div>
+          )}
+        </div>
       </div>
     </div>
   );
