fa5c2b876732c36dbfa94a650ebd86cc6375e409
natreeum <projectkmscom@gmail.com>
2023-01-09 14:11:14 +0900

    feat : mypage base

--- client/src/pages/MyPage.js
+++ client/src/pages/MyPage.js
@@ -1,7 +1,41 @@
 import React from 'react';
 
+import './MyPage.css';
+
 const Mypage = () => {
-  return <div className="Mypage">This is Mypage</div>;
+  return (
+    <div className="mypage">
+      <div className="Mypage">This is Mypage</div>
+      <div className="userInfo">
+        <div className="username">username</div>
+        <div className="userAddress">address</div>
+      </div>
+      <div className="tokenInfo">
+        <div className="balances">
+          <div className="tokenBalance">tokenbalance</div>
+          <div className="etherBalance">etherBalance</div>
+        </div>
+        <div className="transferToken">
+          <input
+            className="receiverAddress"
+            type="text"
+            placeholder="receiverAddress"
+          ></input>
+          <input
+            className="tokenTransferAmount"
+            type="text"
+            placeholder="tokenTransferAmout"
+          ></input>
+          <button className="transferToken">Transfer</button>
+        </div>
+      </div>
+      <div className="myStuffs">
+        <div className="myMissions">myMissions</div>
+        <div className="myComments">myComments</div>
+        <div className="myNFTs">myNFTs</div>
+      </div>
+    </div>
+  );
 };
 
 export default Mypage;
