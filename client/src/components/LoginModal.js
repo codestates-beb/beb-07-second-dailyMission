70dab1dd14b5c9e30a592d6814da291cf75b660a
fromdwlee <desgod@naver.com>
2023-01-10 11:31:31 +0900

    feat: 로그인 정보 local/session storage 에 저장 기능 추가

--- client/src/components/LoginModal.js
+++ client/src/components/LoginModal.js
@@ -9,6 +9,7 @@ function LoginModal(props) {
     const { open, close } = props;
     const [userId, setUserId] = useState("");
     const [password, setPassword] = useState("");
+    const [isChecked, setIsChecked] = useState(false);
     const [signStatus, setSignStatus] = useRecoilState(status);
 
     const onUserIdHandler = (event) => {
@@ -26,13 +27,33 @@ function LoginModal(props) {
 
         })
             .then(res => {
-                console.log(res)
-                const signData = res.data.message;
-                signData['isSigned'] = true;
-                setSignStatus(() => signData)
-                sessionStorage.setItem('signData', JSON.stringify(signData))
+                if (res.data.status === 'falied') {
+                    alert('아이디 또는 비밀번호를 잘못 입력했습니다.')
+                }
+                else if (isChecked === false) {
+                    const signData = res.data.message;
+                    signData['isSigned'] = true;
+                    setSignStatus(() => signData)
+                    sessionStorage.setItem('signData', JSON.stringify(signData))
+                    document.location.href = '/'
+                }
+                else if (isChecked === true) {
+                    const signData = res.data.message;
+                    signData['isSigned'] = true;
+                    setSignStatus(() => signData)
+                    localStorage.setItem('signData', JSON.stringify(signData))
+                    document.location.href = '/'
+                }
             })
     }
+
+    const onClickSignUp = () => {
+        window.location.href = '/signup';
+    }
+
+    const handleChecked = (event) => {
+        setIsChecked(event.target.checked);
+    };
 
     return (
         <div className={open ? 'openModal modal' : 'modal'}>
@@ -45,17 +66,31 @@ function LoginModal(props) {
                         </button>
                     </header>
                     <main>
-                        <input placeholder="아이디" value={userId} onChange={onUserIdHandler}></input><br></br>
-                        <input type="password" placeholder="비밀번호" value={password} onChange={onPasswordHandler}></input>
+                        <div className="main-input">
+                            <input placeholder="아이디" value={userId} onChange={onUserIdHandler}></input><br></br>
+                            <input type="password" placeholder="비밀번호" value={password} onChange={onPasswordHandler}></input>
+                        </div>
+                        <div className="main-btn">
+                            <button onClick={onClickLogin}>
+                                SIGN IN
+                            </button>
+                        </div>
+
                     </main>
                     <footer>
-                        <button onClick={onClickLogin}>
-                            SIGN IN
+                        <div className="footer-check">
+                            <input type='checkbox' onChange={handleChecked} ></input>
+                            <span className="cb-text">로그인 상태 유지</span>
+                        </div>
+                        <button
+                            onClick={onClickSignUp} className="footer-btn">
+                            회원 가입
                         </button>
                     </footer>
                 </section>
-            ) : null}
-        </div>
+            ) : null
+            }
+        </div >
     );
 }
 
